94d01f30a371c3df942034e09c8dd5e0d1091da2
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-10-26 15:23:50 -0700

    working prototype

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+// add front end scripting here!
+const booksUl = document.querySelector("#allBooks");
+const bookForm = document.querySelector("#newBook");
+const bookIdInput = document.querySelector("#bookId");
+const bookTitleInput = document.querySelector("#bookTitle");
+const bookAuthorInput = document.querySelector("#bookAuthor");
+
+
+fetch("/api/books").then(res=>{
+    return res.json()
+}).then(data=>{
+    console.log(data);
+    data.forEach(book=>{
+        const bookLi = document.createElement("li");
+        bookLi.innerHTML=`<strong>${book.id}.</strong> <em>${book.title}</em> by ${book.author}`;
+        booksUl.append(bookLi);
+    })
+})
+
+bookForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    const bookData = {
+        id: parseInt(bookIdInput.value),
+        title: bookTitleInput.value,
+        author: bookAuthorInput.value,
+    }
+    console.log(bookData);
+    fetch("/api/books",{
+        method:"POST",
+        body:JSON.stringify(bookData),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.reload()
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
+
