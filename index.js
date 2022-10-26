07dd0a6cb6d33992c08373a04382259d5d338f5a
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2022-10-26 15:29:52 -0700

    adding dynamic port binding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
